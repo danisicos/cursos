ce2437163d71f689c3051286e84b8c2731549e98
unknown <53632658+danisicos@users.noreply.github.com>
2024-07-21 10:20:35 -0300

    alterando arquivos

--- temperatura.java
+++ temperatura.java
@@ -1,20 +1,14 @@
 import java.util.Scanner;
-
 public class temperatura {
     public static void main (String[] args){
         try (Scanner scanner = new Scanner(System.in)) {
             System.out.println("Digite a temperatura em celsius: ");
             double c = scanner.nextDouble();
-            
-      
             double f = c * 18 + 32;
             double k = c + 273.15;
             double re = c*0.8;
             double ra = c*1.8+32+459.67;
-
-
             System.out.println(c + " em Celsius equivale a " + f + " em Farenheit; " + k + " em Kelvin; " + re + " em Reamur; " + ra + " em Rankine.");
         }
-
     }
 }
--- temperatura.py
+++ temperatura.py
@@ -1,9 +1,7 @@
 class temperatura:
     c = int(input("Digite a temperatura em celsius: "))
-
     f = c * 18 + 32
     k = c + 273.15
     re = c*0.8
     ra = c*1.8+32+459.67
-
     print(f" {c} em Celsius equivale a {f}  em Farenheit; {k} em Kelvin; {re} em Reamur; {ra} em Rankine.")
