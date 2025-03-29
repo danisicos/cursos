569521eed6df73e4a0cc567862e4fab26eb387ca
danisico <daniel85.sc@gmail.com>
2025-03-28 21:34:39 -0300

    Atualizando o repositorio

---
+++ JS/teste.js
@@ -0,0 +1,3 @@
+const num = 1_2_3_4_5 + 1_0;
+
+console.log(num)
---
+++ Java/Exercicios.java
@@ -0,0 +1,12 @@
+public class Exercicios {
+    public static void main(String[] args) {
+        String[] nomes = { "Marta", "Paulo", "Carolina", "Joaquim", "Pedro" };
+
+        int indice = 0;
+        while (indice < nomes.length) {
+            System.out.println(nomes[indice]);
+            indice++;
+        }
+
+    }
+}
